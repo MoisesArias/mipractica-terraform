cc85920d14f3eac967f65cce0db4b1d12ca9b331
Moises Arias <moicarh@gmail.com>
2026-01-21 20:54:54 -0500

    se corrige llamado local

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "storage" {
   source = "./modules/aws/storage"
 
   aws_region  = var.aws_region
-  bucket_name = locals.bucket_name
+  bucket_name = local.bucket_name
   environment = var.environment
   tags        = var.tags
 }
